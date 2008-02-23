047bce111ae991762106f7429d0f8d75ca84dbd8
john.fragkoulis <john.fragkoulis@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-02-23 16:53:01 +0000

    * Added test folder * Added sound test files and three window sounds

---
+++ Tests/Sound/SoundManager.cpp
@@ -0,0 +1,15 @@
+#include "SoundManager.h"
+using namespace game::sound;
+
+SoundManager::SoundManager()
+{}
+
+void SoundManager::init()
+{
+    // 1. Read how many sounds we are going to load
+
+    // 2. Allocate array of N buffers
+    // [if we are using std::map<enum,sound> to load sounds then better allocate with freelist]
+
+    // 3. Initialize buffer objects
+}
---
+++ Tests/Sound/SoundSource.h
@@ -0,0 +1,37 @@
+#ifndef __SOUNDSOURCE_H__
+#define __SOUNDSOURCE_H__
+#pragma once
+
+class SoundSource
+{
+private:
+    typedef unsigned int ALuint;
+    ALuint  m_source;
+    float   m_pitch;
+    float   m_gain;
+    float   m_pos[3];
+    float   m_vel[3];
+    bool    m_loop;
+    bool    m_playing;
+
+public:
+    SoundSource();
+    ~SoundSource();
+
+    bool init(const ALuint * const buffer, const ALuint count = 1);
+    void play();
+    void stop();
+    void pause();
+    void toggle();
+
+    bool isPlaying() const { return m_playing; }
+
+    // Mutators
+    void setLoop( const bool loop );
+    void setPitch( const float pitch );
+    void setGain( const float gain );
+    void setPosition( const float *pos );
+    void setVelocity( const float *vel );
+};
+
+#endif // __SOUNDSOURCE_H__
