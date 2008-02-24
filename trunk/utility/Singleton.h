ff76b847b8e7e4bbabde6ff12f256afeedc7535c
Joep.Moritz <Joep.Moritz@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-02-24 01:33:02 +0000

    Singleton.h magically got deleted. Merged and added again.

---
+++ trunk/utility/Singleton.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <cassert>
+
+template< class T >
+class Singleton
+{
+public:
+
+	//! Returns the object but also checks if the object exists
+    static T& safeInstance()
+    {
+        if (s_instance == NULL) s_instance = new T;
+		return *s_instance;
+	}
+
+	//! Returns the object
+	static T& instance() {
+		assert(s_instance);
+		return *s_instance;
+	}
+
+    //! Destroys the object
+    static void destroy()
+    {
+        delete s_instance;
+        s_instance = 0;
+    }
+    
+protected:
+    // Declare constructor, copy constructor,
+    // assignment operator and destructor protected
+    Singleton() {}
+    Singleton(const Singleton&);
+    Singleton& operator= (const Singleton&);
+    virtual ~Singleton() = 0 {}
+
+    //! The object's unique instance
+    static T* s_instance;
+
+}; // end Singleton
+
+template< class T >
+T* Singleton<T>::s_instance = 0;
