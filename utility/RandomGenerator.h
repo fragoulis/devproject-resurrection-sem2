0af6ee60a51ce6e7b953fb87c4ff50761b9e27f1
manuele.bonanno <manuele.bonanno@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-02-27 21:41:06 +0000

    it's not anymore a class, just a namespace with macros

--- utility/RandomGenerator.h
+++ utility/RandomGenerator.h
@@ -7,39 +7,34 @@
 #include "../Math/Point3.h"
 #include "../Math/Point2.h"
 
-class RandomGenerator: public Singleton<RandomGenerator> {
-public:
+namespace RandomGenerator {
 
-inline float getRandomFloat(float lowBound, float highBound) {
+inline float GET_RANDOM_FLOAT(float lowBound, float highBound) {
 	if( lowBound >= highBound ) 
 		return lowBound;
 	float f = (rand() % 10000) * 0.0001f; 
 	return (f * (highBound - lowBound)) + lowBound; 
 }
 
-inline void getRandomVector4(Vector4 &out, const Vector4 &min, const Vector4 &max) {
-	out.set(getRandomFloat(min.getX(), max.getX()), getRandomFloat(min.getY(), max.getY()), getRandomFloat(min.getZ(), max.getZ()), 1.0f);
+inline void GET_RANDOM_VECTOR4(Vector4 &out, const Vector4 &min, const Vector4 &max) {
+	out.set(GET_RANDOM_FLOAT(min.getX(), max.getX()), GET_RANDOM_FLOAT(min.getY(), max.getY()), GET_RANDOM_FLOAT(min.getZ(), max.getZ()), 1.0f);
 }
 
-inline void getRandomVector3(Vector3 &out, const Vector3 &min, const Vector3 &max) {
-	out.set(getRandomFloat(min.getX(), max.getX()), getRandomFloat(min.getY(), max.getY()), getRandomFloat(min.getZ(), max.getZ()));
+inline void GET_RANDOM_VECTOR3(Vector3 &out, const Vector3 &min, const Vector3 &max) {
+	out.set(GET_RANDOM_FLOAT(min.getX(), max.getX()), GET_RANDOM_FLOAT(min.getY(), max.getY()), GET_RANDOM_FLOAT(min.getZ(), max.getZ()));
 }
 
-inline void getRandomVector2(Vector2 &out, const Vector2 &min, const Vector2 &max) {
-	out.set(getRandomFloat(min.getX(), max.getX()), getRandomFloat(min.getY(), max.getY()));
+inline void GET_RANDOM_VECTOR2(Vector2 &out, const Vector2 &min, const Vector2 &max) {
+	out.set(GET_RANDOM_FLOAT(min.getX(), max.getX()), GET_RANDOM_FLOAT(min.getY(), max.getY()));
 }
 
-inline void getRandomPoint3(Point3 &out, const Point3 &min, const Point3 &max) {
-	out.set(getRandomFloat(min.getX(), max.getX()), getRandomFloat(min.getY(), max.getY()), getRandomFloat(min.getZ(), max.getZ()));
+inline void GET_RANDOM_POINT3(Point3 &out, const Point3 &min, const Point3 &max) {
+	out.set(GET_RANDOM_FLOAT(min.getX(), max.getX()), GET_RANDOM_FLOAT(min.getY(), max.getY()), GET_RANDOM_FLOAT(min.getZ(), max.getZ()));
 }
 
-inline void getRandomPoint2(Point2 &out, const Point2 &min, const Point2 &max) {
-	out.set(getRandomFloat(min.getX(), max.getX()), getRandomFloat(min.getY(), max.getY()));
+inline void GET_RANDOM_POINT2(Point2 &out, const Point2 &min, const Point2 &max) {
+	out.set(GET_RANDOM_FLOAT(min.getX(), max.getX()), GET_RANDOM_FLOAT(min.getY(), max.getY()));
 }
-
-
-private:
-	friend Singleton<RandomGenerator>; 
 
 };
 
