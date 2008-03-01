8d39cda9a97260fd93d8290680d7182e4d7b4099
Joep.Moritz <Joep.Moritz@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-03-01 18:15:15 +0000

    Input fires events when key/mouse buttons go down. Can be turned off with a define. Includes Input.h to get the events.

--- control/Input.cpp
+++ control/Input.cpp
@@ -21,16 +21,28 @@ void Input :: update()
 	{
 		m_previousKeyState[i] = m_keyState[i];
 		if (m_previousKeyState[i] == GOING_DOWN || m_previousKeyState[i] == DOWN)
-			if (m_keyDown[i])
+		{
+			if (m_keyDown[i]) {
 				m_keyState[i] = DOWN;
-			else
+			}
+			else {
 				m_keyState[i] = GOING_UP;
+			}
+		}
 
 		if (m_previousKeyState[i] == GOING_UP || m_previousKeyState[i] == UP)
-			if (m_keyDown[i])
+		{
+			if (m_keyDown[i]) {
 				m_keyState[i] = UP;
-			else
+			}
+			else {
 				m_keyState[i] = GOING_DOWN;
+#ifdef INPUT_FIRES_EVENTS
+				if (i < 3) EventManager::instance().fireEvent(Mouse_GoingDown(i));
+				else EventManager::instance().fireEvent(Key_GoingDown(i));
+#endif
+			}
+		}
 	}
 
 
