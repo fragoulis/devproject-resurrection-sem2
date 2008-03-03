834d638dd82d8c76d980c828269b471a754b47d1
esrever2357 <esrever2357@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-03-03 10:00:53 +0000

    Changed a bit the Coordinate frame - no worries, works as before
    More basic Particle system stuff

--- trunk/ParticleSystem/PS_Manager.cpp
+++ trunk/ParticleSystem/PS_Manager.cpp
@@ -1,9 +1,11 @@
 #include "PS_Manager.h"
 #include "PS_Base.h"
+#include "../gfx/VBO/VBO.h"
 
 using namespace std;
 
 PS_Manager :: PS_Manager()
+:m_vbo(0)
 {
 }
 
@@ -13,12 +15,12 @@ PS_Manager :: ~PS_Manager()
 
 void PS_Manager :: clear()
 {
-	for(vector<PS_Base *>::iterator it = _psList.begin();
-		it != _psList.end();
+	for(vector<PS_Base *>::iterator it = m_psList.begin();
+		it != m_psList.end();
 		++it)
 	{
 		delete (*it);
 		(*it) = 0;
 	}
-	_psList.clear();
+	m_psList.clear();
 }
