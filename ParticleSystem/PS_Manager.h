12c2021da5f75c048838d4d350d4e6aae0b3f781
esrever2357 <esrever2357@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-03-03 10:59:20 +0000

    a few updates more, on dummy PS mainly, making the structure of the manager. Added one more dummy PS in config.

--- ParticleSystem/PS_Manager.h
+++ ParticleSystem/PS_Manager.h
@@ -6,15 +6,30 @@
 class PS_Base;
 class VBO;
 class ParserSection;
+
 class PS_Manager : public Singleton<PS_Manager>
 {
 public :
 	void clear();
 	void init(const ParserSection * parsec);	
+
+	// fetchNewPS : gets an dynamically allocated PS based on a random template, fast - no worries
+	// if needed , the mem allocator could be changed to the faster MemMgrAny<>, but not really needed.
+	PS_Base * fetchNewPS(const std::string& name); 
 	
 
 private : 
+	struct PSNameVariations
+	{
+		std::string name;				// name of ps
+		unsigned	variations;			// number of variations stored
+		unsigned	posInList;			// first index in list
+		PSNameVariations(const std::string& n, const unsigned var,const unsigned pos)
+			:name(n),variations(var),posInList(pos){}
+	};
+
 	std::vector<PS_Base *>	m_psList;
+	std::vector<PSNameVariations> m_psDescList;
 	VBO					  * m_vbo;
 
 	// singleton stuff
