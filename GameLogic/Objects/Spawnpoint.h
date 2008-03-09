3a8ec3687ce4a5c70344ebabbb10dd9e8887ed8c
Joep.Moritz <Joep.Moritz@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-03-09 18:14:49 +0000

    Added more options to Spawnpoint

--- GameLogic/Objects/Spawnpoint.h
+++ GameLogic/Objects/Spawnpoint.h
@@ -11,6 +11,7 @@
 #pragma once
 #include "../WorldObject.h"
 class ParserSection;
+class Point3;
 
 class Spawnpoint : public WorldObject
 {
@@ -18,13 +19,34 @@ public:
 	Spawnpoint();
 	virtual ~Spawnpoint();
 
-	void update(float dt);
+	void update(float dt, const Point3& playerPosition);
 
 	void loadSettings(const ParserSection&);
 
 private:
 	int m_spawnType;
 	int m_spawnCount;
-	float m_spawnPeriodTime;
-	float m_timeTillNextSpawn;
+	float m_timeBetweenInRangeAndFirstSession;
+	float m_timeBetweenSessionStartAndFirstSpawn;
+	float m_timeBetweenSpawns;
+	float m_timeBetweenLastSpawnAndSessionEnd;
+	float m_timeBetweenSessionEndAndSessionStart;
+	float m_minimumPlayerDistance;
+	float m_maximumPlayerDistance;
+
+
+	enum State
+	{
+		WAITING_FOR_PLAYER,
+		SESSION_STARTING,
+		SPAWNING,
+		SESSION_ENDING,
+		WAITING_BETWEEN_SESSIONS,
+	};
+
+	State m_state;
+	float m_timeTillNextEvent;
+	int m_enemiesLeftToSpawnThisSession;
+
+	void _spawnEnemy();
 };
