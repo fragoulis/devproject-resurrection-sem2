a2397b82090a16712be0544c3695157c34de6780
Joep.Moritz <Joep.Moritz@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-03-21 15:31:26 +0000

    Added code for leak detection (win32 and VLD), but commented it out. Changed ControllerManager to not hold a list of controllers.

--- trunk/control/ControllerManager.cpp
+++ trunk/control/ControllerManager.cpp
@@ -10,75 +10,10 @@
 
 #include "ControllerManager.h"
 #include "IController.h"
-#include "LoadingController.h"
-#include "GameController.h"
-#include "MenuController.h"
+//#include "LoadingController.h"
+//#include "GameController.h"
+//#include "MenuController.h"
 #include <cassert>
-
-ControllerManager :: ControllerManager() : m_activeController(NULL)
-{
-	IController* c = createController("empty");
-	addController("empty", c);
-	m_activeController = c;
-}
-
-ControllerManager :: ~ControllerManager()
-{
-}
-
-
-void ControllerManager :: activateController(const std::string& name)
-{
-	IController* c = findController(name);
-	if (c == NULL) {
-		c = createController(name);
-		assert(c != NULL);
-		addController(name, c);
-	}
-	activateController(c);
-}
-
-void ControllerManager :: loadController(const std::string& name)
-{
-	IController* c = findController(name);
-	if (c == NULL) {
-		c = createController(name);
-		assert(c != NULL);
-		addController(name, c);
-	}
-}
-
-void ControllerManager :: unloadController(const std::string& name)
-{
-	IController* c = findController(name);
-	assert(c != m_activeController);
-	if (c != NULL) {
-		removeController(name);
-		delete c;
-	}
-}
-
-void ControllerManager :: unloadAllControllers()
-{
-	struct ControllerDeleter
-	{
-		void operator () (const std::string&, IController* c) { delete c; }
-	};
-	m_controllers.visitObjects(ControllerDeleter());
-}
-
-
-void ControllerManager :: activateController(IController* c)
-{
-	m_activeController->deactivate();
-	m_activeController = c;
-	m_activeController->activate();
-}
-
-void ControllerManager::update(float dt)
-{
-	m_activeController->update(dt);
-}
 
 class EmptyController : public IController
 {
@@ -90,11 +25,91 @@ public:
 	virtual void update(float dt) { }
 };
 
-IController* ControllerManager::createController(const std::string &name) const
+
+ControllerManager :: ControllerManager() : m_activeController(NULL)
 {
-	if (name == "empty") return new EmptyController();
-	if (name == "loading") return new LoadingController();
-	if (name == "game") return new GameController();
-	if (name == "menu") return new MenuController();
-	return NULL;
+	//IController* c = createController("empty");
+	//addController("empty", c);
+	//m_activeController = c;
+	m_activeController = new EmptyController();
 }
+
+ControllerManager :: ~ControllerManager()
+{
+	m_activeController->deactivate();
+	delete m_activeController;
+}
+
+void ControllerManager::update(float dt)
+{
+	m_activeController->update(dt);
+}
+
+
+void ControllerManager :: activateController(IController* controller)
+{
+	m_activeController->deactivate();
+	delete m_activeController;
+	m_activeController = controller;
+	m_activeController->activate();
+}
+
+
+//void ControllerManager :: activateController(const std::string& name)
+//{
+//	IController* c = findController(name);
+//	if (c == NULL) {
+//		c = createController(name);
+//		assert(c != NULL);
+//		addController(name, c);
+//	}
+//	activateController(c);
+//}
+//
+//void ControllerManager :: loadController(const std::string& name)
+//{
+//	IController* c = findController(name);
+//	if (c == NULL) {
+//		c = createController(name);
+//		assert(c != NULL);
+//		addController(name, c);
+//	}
+//}
+//
+//void ControllerManager :: unloadController(const std::string& name)
+//{
+//	IController* c = findController(name);
+//	assert(c != m_activeController);
+//	if (c != NULL) {
+//		removeController(name);
+//		delete c;
+//	}
+//}
+//
+//void ControllerManager :: unloadAllControllers()
+//{
+//	struct ControllerDeleter
+//	{
+//		void operator () (const std::string&, IController* c) { delete c; }
+//	};
+//	m_controllers.visitObjects(ControllerDeleter());
+//}
+//
+//
+//void ControllerManager :: activateController(IController* c)
+//{
+//	m_activeController->deactivate();
+//	m_activeController = c;
+//	m_activeController->activate();
+//}
+//
+//
+//
+//IController* ControllerManager::createController(const std::string &name) const
+//{
+//	if (name == "empty") return new EmptyController();
+//	if (name == "loading") return new LoadingController();
+//	if (name == "game") return new GameController();
+//	if (name == "menu") return new MenuController();
+//	return NULL;
+//}
