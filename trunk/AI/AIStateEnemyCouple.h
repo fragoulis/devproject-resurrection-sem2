392e6722e5731ba6e128e53e949d974083cc748f
john.fragkoulis <john.fragkoulis@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-04-03 14:27:25 +0000

    moved AIStateEnemyCouple to its own class

---
+++ trunk/AI/AIStateEnemyCouple.h
@@ -0,0 +1,34 @@
+#pragma once
+class IAIState;
+class Enemyship;
+class Playership;
+class Point3;
+class Vector3;
+
+/**
+ * Holds info about enemies and states.
+ * According to this structure each enemy has one state and this
+ * state is interchangable with any other from within its update.
+ */
+class AIStateEnemyCouple
+{
+private:
+    IAIState *m_state;
+    Enemyship *m_enemy;
+    float m_timeToChange;
+    float m_currentTime;
+
+public:
+    AIStateEnemyCouple();
+    void update( Playership *player, float dt );
+    bool readyToChange( float dt );
+    
+    void setTimeToChange( float t ) { m_timeToChange = t; }
+    const Point3& getEnemyPosition() const;
+    void setEnemyThrusterDirection( const Vector3& dir );
+
+    void setEnemyship( Enemyship *enemy ) { m_enemy = enemy; }
+    void setState( IAIState *state ) { m_state = state; }
+
+    const Enemyship* getEnemyship() const { return m_enemy; }
+};
