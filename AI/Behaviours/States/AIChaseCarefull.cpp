2ce1cbe11905bedabb4f82042f3f042409e7408f
john.fragkoulis <john.fragkoulis@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-04-13 16:01:10 +0000

    * corrected chase careful

--- AI/Behaviours/States/AIChaseCarefull.cpp
+++ AI/Behaviours/States/AIChaseCarefull.cpp
@@ -7,5 +7,13 @@ void AIChaseCarefull::update( float dt, Playership *player, AIEnemy *enemy )
     // Get direction to the player ship
     Vector3 dir = player->getPosition() - enemy->getEnemyPosition();
     dir.normalize();
+
+    const Vector3 &player_dir = player->getThrusterDirection();
+    const float per = player_dir.dot( dir );
+
+    const Rotation r(Vector3(0.0f, 1.0f, 0.0f), per);
+    r.applyTo(dir);
+
+
     enemy->setEnemyThrusterDirection( dir );
 }
