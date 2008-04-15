86ce2fc88205fd0218e613b3be74e9d07f7239f4
john.fragkoulis <john.fragkoulis@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-04-15 11:44:36 +0000

    * made three numbers float to remove warnings

--- Menu/MainPage.cpp
+++ Menu/MainPage.cpp
@@ -2,21 +2,21 @@
 
 void MainPage::init(int screenWidth, int screenHeight) {
 	MenuItem *startButton = new MenuItem();
-	startButton->init(screenWidth/2-256, 300, 256, 100, "menuPlay.bmp", "menuPlaySel.bmp", MenuItem::ITEM_STATE_SELECTED);
+	startButton->init(screenWidth/2.0f-256, 300, 256, 100, "menuPlay.bmp", "menuPlaySel.bmp", MenuItem::ITEM_STATE_SELECTED);
 	m_items.push_back(startButton);
 
 	MenuItem *creditsButton = new MenuItem();
-	creditsButton->init(screenWidth/2-256, 200, 256, 100, "menuCredits.bmp", "menuCreditsSel.bmp", MenuItem::ITEM_STATE_UNSELECTED);
+	creditsButton->init(screenWidth/2.0f-256, 200, 256, 100, "menuCredits.bmp", "menuCreditsSel.bmp", MenuItem::ITEM_STATE_UNSELECTED);
 	m_items.push_back(creditsButton);
 
 	MenuItem *exitButton = new MenuItem();
-	exitButton->init(screenWidth/2-256, 100, 256, 100, "menuExit.bmp", "menuExitSel.bmp", MenuItem::ITEM_STATE_UNSELECTED);
+	exitButton->init(screenWidth/2.0f-256, 100, 256, 100, "menuExit.bmp", "menuExitSel.bmp", MenuItem::ITEM_STATE_UNSELECTED);
 	m_items.push_back(exitButton);
 
 
 	m_backgroundImage = TextureIO::instance()->getTexture("menuBg.bmp");
 	m_hasBackground = true;
-
+ 
 	m_screenWidth = screenWidth;
 	m_screenHeight = screenHeight;
 }
