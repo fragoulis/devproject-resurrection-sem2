045823dedd089baf6c9cf0dc1d381be84f089424
john.fragkoulis <john.fragkoulis@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-04-16 20:51:01 +0000

    * added some missing functionality to base sound classes

--- Sound/Sound.h
+++ Sound/Sound.h
@@ -13,10 +13,14 @@ public:
     Sound();
     Sound( SoundBuffer* );
 
+    unsigned getSource() const { return m_source.GetSource(); }
+    unsigned getBuffer() const;
+
     void set( SoundBuffer* );
     void update( const Point3& );
 
     void play() const        { m_source.play(); }
+    void stop() const        { m_source.stop(); }
     void setPitch( float p ) { m_source.setPitch(p); }
     void setGain( float g )  { m_source.setGain(g); }
     void setLoop( bool l )   { m_source.setLoop(l); }
