184aac2b14b9399a01a9999ea01072838bf48a9a
john.fragkoulis <john.fragkoulis@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-04-26 14:02:31 +0000

    * changed the 'height' of the menu items

--- trunk/Menu/PausePage.cpp
+++ trunk/Menu/PausePage.cpp
@@ -1,16 +1,22 @@
 #include "PausePage.h"
 
-void PausePage::init(int screenWidth, int screenHeight) {
+void PausePage::init(int screenWidth, int screenHeight) 
+{
+    const float posX = screenWidth * 0.5f - 128.0f;
+    const float startPosY = 400.0f, deltaY = 65.0f;
+    const int width  = 256;
+    const int height = 64;
+
 	MenuItem *resumeButton = new MenuItem();
-	resumeButton->init(screenWidth/2.0f-128, 450, 256, 100, "menuResume.bmp", "menuResumeSel.bmp", MenuItem::ITEM_STATE_SELECTED);
+	resumeButton->init(posX, startPosY, width, height, "menuResume.bmp", "menuResumeSel.bmp", MenuItem::ITEM_STATE_SELECTED);
 	m_items.push_back(resumeButton);
 
 	MenuItem *controlsButton = new MenuItem();
-	controlsButton->init(screenWidth/2.0f-128, 350, 256, 100, "menuControls.bmp", "menuControlsSel.bmp", MenuItem::ITEM_STATE_UNSELECTED);
+	controlsButton->init(posX, startPosY-deltaY, width, height, "menuControls.bmp", "menuControlsSel.bmp", MenuItem::ITEM_STATE_UNSELECTED);
 	m_items.push_back(controlsButton);
 
 	MenuItem *exitButton = new MenuItem();
-	exitButton->init(screenWidth/2.0f-128, 250, 256, 100, "menuExitPause.bmp", "menuExitPauseSel.bmp", MenuItem::ITEM_STATE_UNSELECTED);
+	exitButton->init(posX, startPosY-2*deltaY, width, height, "menuExitPause.bmp", "menuExitPauseSel.bmp", MenuItem::ITEM_STATE_UNSELECTED);
 	m_items.push_back(exitButton);
 
 	m_hasBackground = false;
