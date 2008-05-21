87770b457e2d20902efa7029461cb098d4ec7b6c
Joep.Moritz <Joep.Moritz@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-05-21 20:37:28 +0000

    Removed debug files from camera test.

--- branches/gamecube-version/TestApps/CameraTest/GameCube Elf3/NGC_Debug/main.d
+++
@@ -1,4 +0,0 @@
-NGC_Debug/main.obj: \
- "g:/Visual Studio 2005/Projects/GameCube Elf3/GameCube Elf3/main.cpp" \
- "g:/Visual Studio 2005/Projects/GameCube Elf3/GameCube Elf3/Camera.h" \
- "g:/Visual Studio 2005/Projects/GameCube Elf3/GameCube Elf3/Vector3.h"
