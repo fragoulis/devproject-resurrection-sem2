1ef88170621a27b304985a7702d68c299148f68d
Joep.Moritz <Joep.Moritz@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-05-22 22:12:53 +0000

    Added assert.h, ObjectReferenceMap, ConfParser and Misc utils.

---
+++ branches/gamecube-version/gfxutils/Misc/utils.cpp
@@ -0,0 +1,142 @@
+#include "../Misc/utils.h"
+#include <string>
+//#include <gl/glee.h>
+//#include <GL/glu.h>
+#include <iostream>
+
+using namespace std;
+
+
+
+
+std::string getExtension(const char *fname)
+{
+	char out[512];
+	unsigned i=0,j=0;
+	unsigned last_found = 0;
+	while(fname[i] != 0)
+	{
+		if(fname[i] == '.')
+			last_found = i;
+		++i;
+	}
+	i=last_found+1;
+	while(fname[i] != 0)
+		out[j++] =fname[i++];
+	out[j] = 0;
+	return string(out);
+}
+
+std::string truncDir(const std::string& full)
+{
+	size_t pos = full.find_last_of("/\\");
+	if(pos != string::npos)
+		return full.substr(pos+1,full.size() - pos-1);
+	else
+		return full;
+}
+
+bool isPOT(const unsigned val)
+{
+	unsigned i=2;
+	while(i<val)
+	{i<<=1;}
+	return (i == val);
+}
+
+unsigned nearestPOT(const unsigned number)
+{
+	unsigned test = 1;
+	while(test < number)
+		test<<=1;
+	return test;
+}
+
+
+
+
+
+
+
+
+//// GL ERROR CHECK
+//int CheckGLError(char *file, int line)
+//{
+//	GLenum glErr;
+//	int    retCode = 0;
+//
+//	glErr = glGetError();
+//	while (glErr != GL_NO_ERROR) 
+//   {
+//       cerr << "GL Error #" << glErr << "(" << gluErrorString(glErr) << ") " << " in File " << file << " at line: " << line << endl;
+//		retCode = 1;
+//		glErr = glGetError();
+//		assert(0);
+//	}
+//	return retCode;
+//}
+//
+//// GL ENUMERATION of IFMT & DTYPE
+//unsigned componentsByInternalFormat(const GLuint ifmt)
+//{
+//	switch(ifmt)
+//	{
+//		case 1 :
+//		case GL_LUMINANCE :
+//		case GL_SLUMINANCE :
+//		case GL_ALPHA :
+//		case GL_DEPTH_COMPONENT:
+//		case GL_DEPTH_COMPONENT16:
+//		case GL_DEPTH_COMPONENT24:
+//		case GL_DEPTH_COMPONENT32:
+//
+//			return 1;
+//		
+//		case 2 :
+//		case GL_LUMINANCE_ALPHA :
+//		case GL_SLUMINANCE_ALPHA :
+//			return 2;
+//
+//		case 3 :
+//		case GL_RGB :
+//		case GL_RGB8 :
+//		case GL_SRGB :
+//		case GL_SRGB8 :
+//		case GL_COMPRESSED_RGB_S3TC_DXT1_EXT :
+//			return 3;
+//
+//		case 4:
+//		case GL_RGBA :
+//		case GL_SRGB_ALPHA :
+//		case GL_SRGB8_ALPHA8 :
+//		case GL_COMPRESSED_RGBA_S3TC_DXT1_EXT :
+//		case GL_COMPRESSED_RGBA_S3TC_DXT3_EXT :
+//		case GL_COMPRESSED_RGBA_S3TC_DXT5_EXT :
+//			return 4;
+//
+//		default :
+//			assert(0);
+//	}
+//	return 0;
+//}
+//
+//unsigned BytesPerComponentByDatatype(const GLuint dtype)
+//{
+//	switch(dtype)
+//	{
+//		case GL_UNSIGNED_BYTE:
+//		case GL_BYTE:
+//			return 1;
+//		case GL_UNSIGNED_INT :
+//		case GL_INT :
+//			return sizeof(GLint);
+//
+//		case GL_FLOAT : 
+//			return sizeof(GLfloat);
+//		
+//		default :
+//			assert(0);
+//	}
+//	return 0;
+//}
+//
---
+++ branches/gamecube-version/gfxutils/Misc/utils.h
@@ -0,0 +1,29 @@
+#ifndef _RES_UTILS_H_
+#define _RES_UTILS_H_
+
+#include <string>
+#include <sstream>
+//#include <assert.h>
+//#include <gl/glee.h>
+
+//#ifdef _DEBUG
+//#define CHECK_GL_ERROR() CheckGLError(__FILE__, __LINE__)
+//#else
+//#define CHECK_GL_ERROR() 
+//#endif
+
+
+
+std::string getExtension(const char *fname);	// Gets a filename's extension
+std::string truncDir(const std::string& full);
+bool isPOT(const unsigned val);		// Checks if the value is a power of 2
+unsigned nearestPOT(const unsigned number);
+int CheckGLError(char *file, int line);	// Checks for active GL errors
+
+//unsigned componentsByInternalFormat(const GLuint ifmt);
+//unsigned BytesPerComponentByDatatype(const GLuint dtype);
+
+#include "../Misc/utils.hpp"
+
+
+#endif
