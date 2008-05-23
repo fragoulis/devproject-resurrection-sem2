540609fee6368fe02ecbeeed85483354c90c4ee0
john.fragkoulis <john.fragkoulis@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-05-23 17:55:34 +0000

    added sound engine and sound ids

---
+++ branches/gamecube-version/sound/sounds.h
@@ -0,0 +1,20 @@
+//  SP Demo Application
+//  (c) Copyright 2001 Nintendo Technology Development, Inc.
+//  
+#define SFX_LASER			0
+#define SFX_IFACE_BKG		1
+#define SFX_IFACE_CLICK		2
+#define SFX_EBOMB_BEEP		3
+#define SFX_EBOMB_EXP		4
+#define SFX_EBOMB_HOLLOW	5
+#define SFX_EBOMB_READY		6
+#define SFX_EBOMB_SUCCEDED	7
+#define SFX_FIREBALL		8
+#define SFX_EXP01			9
+#define SFX_EXP02			10
+#define SFX_PLAYER_LOST		11
+#define SFX_LEVEL_COMPLETE	12
+#define SFX_WELCOME			13
+#define SFX_DOT				14
+#define SFX_STRACK1			15
+#define SFX_STRACK2			16
