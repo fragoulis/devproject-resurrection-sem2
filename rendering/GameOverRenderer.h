36c34a25c5ad5c0114e00456ebf27e45d24b2f0c
Joep.Moritz <Joep.Moritz@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-05-24 01:46:34 +0000

    Added Menu, Renderers.

---
+++ rendering/GameOverRenderer.h
@@ -0,0 +1,25 @@
+#ifndef _RES_GAME_OVER_RENDERER_H_
+#define _RES_GAME_OVER_RENDERER_H_
+#include "IRenderer.h"
+class Texture;
+
+/**
+ * Renders the game over text
+ *
+ */
+class GameOverRenderer : public IRenderer
+{
+public:
+	GameOverRenderer();
+	virtual ~GameOverRenderer();
+
+	virtual void render(Graphics& g) const;
+	virtual void update(float dt);
+
+private:
+	//Texture* m_texture;
+
+};
+
+
+#endif
