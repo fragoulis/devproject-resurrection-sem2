5a4170753b509143ea2ed9cacf791ecf24f578e4
john.fragkoulis <john.fragkoulis@cd4a7bbe-ff45-0410-80bb-93172b37cb13>
2008-05-24 17:31:04 +0000

    updated sound engine

--- branches/gamecube-version/sound/SoundEngine.cpp
+++ branches/gamecube-version/sound/SoundEngine.cpp
@@ -8,7 +8,7 @@
 #include "../gfxutils/Misc/utils.h"
 #include <iostream>
 
-VOICE SoundEngine::_voices[MAX_VOICES];
+VOICE SoundEngine::m_voices[MAX_VOICES];
 
 SoundEngine :: SoundEngine():
 m_listener(0),
@@ -27,6 +27,7 @@ m_activeEplosions(0)
 	EventManager::instance().registerEventListener< Level_Unload >(this);
     EventManager::instance().registerEventListener< Game_Over >(this);
     EventManager::instance().registerEventListener< Level_Complete >(this);
+	EventManager::instance().registerEventListener< Button_GoingDown >(this);
 }
 
 SoundEngine :: ~SoundEngine()
@@ -39,7 +40,11 @@ SoundEngine :: ~SoundEngine()
 // Load most sounds, interface and game sounds, except the ambient background ones
 void SoundEngine :: onApplicationLoad(const ParserSection& ps)
 {
-	ARInit(aramMemArray, MAX_ARAM_BLOCKS);
+	cout << "Initializing sound engine ... " << endl;
+
+	ARInit(m_aramMemArray, MAX_ARAM_BLOCKS);
+	//cout << "ARInit returned: " << ret << endl;
+
 	ARQInit();
 
     // initialize AI subsystem
@@ -54,45 +59,82 @@ void SoundEngine :: onApplicationLoad(const ParserSection& ps)
     // -----------------------------------------------------------
 
     // get a block from the AR ARAM allocator
-    aramUserBase = ARAlloc(AUDIO_BLOCK_SIZE_BYTES);
+    m_aramUserBase = ARAlloc(AUDIO_BLOCK_SIZE_BYTES);
+	//cout << "ARAlloc returned: " << m_aramUserBase << endl;
 
     // initialize AM with the block
-    AMInit(aramUserBase, AUDIO_BLOCK_SIZE_BYTES);
+    AMInit(m_aramUserBase, AUDIO_BLOCK_SIZE_BYTES);
 
     // retrieve start of zero buffer, as created by AM
-    aramZeroBase = AMGetZeroBuffer();
+    m_aramZeroBase = AMGetZeroBuffer();
+	//cout << "AMGetZeroBuffer returned: " << m_aramZeroBase << endl;
 
 	// -----------------------------------------------------------
     // Load SP data!
     // -----------------------------------------------------------
 
     // Retrieve sound table 
-    sp_table = (SPSoundTable *)AMLoadFile(SPT_FILE, NULL);
+    m_sp_table = (SPSoundTable *)AMLoadFile(SPT_FILE, NULL);
 
     // Load sound effects into ARAM
-    aramUserBase = AMPushBuffered(SPD_FILE, (void *)xfer_buffer, XFER_BUFFER_SIZE_BYTES);
+    m_aramUserBase = AMPushBuffered(SPD_FILE, (void *)m_xfer_buffer, XFER_BUFFER_SIZE_BYTES);
+	//cout << "AMPushBuffered returned: " << m_aramUserBase << endl;
 
     // -----------------------------------------------------------
     // initialize sound table! 
     // -----------------------------------------------------------
-    SPInitSoundTable(sp_table, aramUserBase, aramZeroBase);
+    SPInitSoundTable(m_sp_table, m_aramUserBase, m_aramZeroBase);
 
 	for (int i=0; i<MAX_VOICES; i++)
 	{
-		_voices[i].ax_voice = NULL;
-		_voices[i].sp_entry = NULL;
+		m_voices[i].ax_voice = NULL;
+		m_voices[i].sp_entry = NULL;
 	}
 
 	AXRegisterCallback(ax_demo_callback);
 
     // initialize profiling for AX
     AXInitProfile(ax_profile, NUM_AX_PROFILE_FRAMES); 
+
+	_matchSoundIdToString();
+
+	cout << "Sound engine initialized!" << endl;
 }
 
 // ----------------------------------------------------------------------------
 // clear all sounds and buffers and ambient buffer
 void SoundEngine :: onApplicationUnload()
 {
+}
+
+// ----------------------------------------------------------------------------
+void SoundEngine::_matchSoundIdToString()
+{
+#define PUSH_PAIR(key, value) m_sounds.insert( make_pair(key, value) );
+
+	PUSH_PAIR("Laser",			SFX_LASER);	
+	PUSH_PAIR("IFaceBgk",		SFX_IFACE_BKG);
+	PUSH_PAIR("IFaceClick",		SFX_IFACE_CLICK);
+	PUSH_PAIR("EbombBeep",		SFX_EBOMB_BEEP);
+	PUSH_PAIR("EbombExp",		SFX_EBOMB_EXP);
+	PUSH_PAIR("EbombHollow",	SFX_EBOMB_HOLLOW);
+	PUSH_PAIR("EbombReady",		SFX_EBOMB_READY);
+	PUSH_PAIR("EbombSucceded",	SFX_EBOMB_SUCCEDED);
+	PUSH_PAIR("Fireball",		SFX_FIREBALL);
+	PUSH_PAIR("Exp01",			SFX_EXP01);
+	PUSH_PAIR("Exp02",			SFX_EXP02);
+	PUSH_PAIR("PlayerLost",		SFX_PLAYER_LOST);
+	PUSH_PAIR("LevelComplete",	SFX_LEVEL_COMPLETE);
+	PUSH_PAIR("Welcome",		SFX_WELCOME);
+	PUSH_PAIR("Dot",			SFX_DOT);
+
+#undef PUSH_PAIR
+}
+
+// ----------------------------------------------------------------------------
+void SoundEngine :: onEvent(Button_GoingDown& ev)
+{
+	play("Welcome");
 }
 
 // ----------------------------------------------------------------------------
@@ -264,9 +306,10 @@ void SoundEngine::clearSoundPositions()
 void SoundEngine::play( const string &id, bool repeat )
 {
 	const int sound = m_sounds[id];
-	if(repeat) play_sfx(sound);
+	std::cout << "Trying to play sound[\""<< id << "\"]=" << sound << std::endl;
+	if(repeat) play_looped_sfx(sound);
 	else
-		play_looped_sfx(sound);
+		play_sfx(sound);
 }
 
 // ----------------------------------------------------------------------------
@@ -278,9 +321,9 @@ void SoundEngine::stop( unsigned id )
 
     for (int i=0; i<MAX_VOICES; i++)
     {
-        if (_voices[i].ax_voice)
+        if (m_voices[i].ax_voice)
         {
-            AXSetVoiceState(_voices[i].ax_voice, AX_PB_STATE_STOP);
+            AXSetVoiceState(m_voices[i].ax_voice, AX_PB_STATE_STOP);
         }
     }
 
@@ -290,18 +333,16 @@ void SoundEngine::stop( unsigned id )
 // ----------------------------------------------------------------------------
 void SoundEngine::ax_demo_callback(void)
 {
-
     u32 i;
-
     for (i=0; i<MAX_VOICES; i++)
     {
-        if (_voices[i].ax_voice)
+        if (m_voices[i].ax_voice)
         {
-            if ( AX_PB_STATE_STOP == ((_voices[i].ax_voice)->pb.state))
+            if ( AX_PB_STATE_STOP == ((m_voices[i].ax_voice)->pb.state))
             {
-                MIXReleaseChannel(_voices[i].ax_voice);
-                AXFreeVoice(_voices[i].ax_voice);
-                _voices[i].ax_voice = NULL;
+                MIXReleaseChannel(m_voices[i].ax_voice);
+                AXFreeVoice(m_voices[i].ax_voice);
+                m_voices[i].ax_voice = NULL;
             }
         }
     }
@@ -311,16 +352,20 @@ void SoundEngine::ax_demo_callback(void)
 void SoundEngine::play_sfx(u32 sfx)
 {
     BOOL old = OSDisableInterrupts();
-    VOICE *v = get_voice();
+	//cout << "OSDisableInterrupts() returned: " << old << endl;
+    
+	VOICE *v = get_voice();
 
     if (v)
     {
+		//cout << "Found an available voice" << endl;
 
         v->ax_voice = AXAcquireVoice(15, ax_drop_voice_callback, 0);
         if (v->ax_voice)
         {
+			//cout << "Acquired the voice" << endl;
 
-            v->sp_entry = SPGetSoundEntry(sp_table, sfx);
+            v->sp_entry = SPGetSoundEntry(m_sp_table, sfx);
 
             SPPrepareSound(v->sp_entry, v->ax_voice, (v->sp_entry)->sampleRate);
 
@@ -328,19 +373,17 @@ void SoundEngine::play_sfx(u32 sfx)
             AXSetVoiceState(v->ax_voice, AX_PB_STATE_RUN);
 
             OSRestoreInterrupts(old);
-
         }
         else
         {
+			std::cout << "Couldnt acquire the available voice" << std::endl;
             OSRestoreInterrupts(old);
-            //DEMOWinLogPrintf(DebugWin, "SFX: AX Voice allocation failed.\n");
         }
-
     }
     else
     {
+		cout << "Couldn't find an available voice" << endl;
         OSRestoreInterrupts(old);
-        //DEMOWinLogPrintf(DebugWin, "(No free voices in abstraction layer)\n");
     }
 
 } // end play_sfx()
@@ -357,7 +400,7 @@ void SoundEngine::play_looped_sfx(u32 sfx)
         v->ax_voice = AXAcquireVoice(15, ax_drop_voice_callback, 0);
         if (v->ax_voice)
         {
-            v->sp_entry = SPGetSoundEntry(sp_table, sfx);
+            v->sp_entry = SPGetSoundEntry(m_sp_table, sfx);
 			v->ax_voice->pb.addr.loopFlag = AXPBADDR_LOOP_ON;
 			v->sp_entry->type = SP_TYPE_PCM16_LOOPED;
 			v->sp_entry->loopAddr = v->sp_entry->currentAddr;
@@ -389,21 +432,17 @@ void SoundEngine::play_looped_sfx(u32 sfx)
 // ----------------------------------------------------------------------------
 VOICE *SoundEngine::get_voice()
 {
-
-    u32 i;
-
-        i=0;
-        while (i < MAX_VOICES)
+    u32 i = 0;
+    while (i < MAX_VOICES)
+    {
+        if (NULL == m_voices[i].ax_voice)
         {
-
-            if (NULL == _voices[i].ax_voice)
-            {
-                return(&_voices[i]);
-            }
-            i++;
+            return(&m_voices[i]);
         }
+        i++;
+    }
 
-        return(NULL);
+    return(NULL);
 
 }  // end get_demo_voice()
 
@@ -416,12 +455,12 @@ void SoundEngine::ax_drop_voice_callback(void *p)
     for (i=0; i<MAX_VOICES; i++)
     {
         // found it! 
-        if  ( (AXVPB *)(p) == _voices[i].ax_voice)
+        if  ( (AXVPB *)(p) == m_voices[i].ax_voice)
         {
             // release mixer channel, delete reference to AX voice (and SP entry, just for neatness)
-            MIXReleaseChannel(_voices[i].ax_voice);
-            _voices[i].ax_voice = NULL;
-            _voices[i].sp_entry = NULL;
+            MIXReleaseChannel(m_voices[i].ax_voice);
+            m_voices[i].ax_voice = NULL;
+            m_voices[i].sp_entry = NULL;
             break;
         }
     }
